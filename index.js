8a72c52b676cabb793d8e9bf6c12ea1f9bf49286
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-18 09:10:04 +0100

    Add square click event listener

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let Squares = []
+class Square {
+    clickCount = 0;
+}
+
 main();
 
 function main() {
@@ -5,18 +10,29 @@ function main() {
 }
 
 function createSquare() {
+    Squares.push(new Square());
     var div = document.createElement("div");
     div.style.width = "50px"
     div.style.height = "50px";
     div.style.background = "black";
+    div.id = (Squares.length - 1).toString();
+    div.onclick = squareClicked;
     return div;
 }
 
 function fillBoard(size) {
+    Squares = [];
     const board = document.getElementById("board");
     for (let index = 0; index < size; index++) {
-        const s = createSquare();
-        console.log(index, s);
-        board.appendChild(s);
+        const sq = createSquare();
+        console.log(index, sq);
+        board.appendChild(sq);
     }
+}
+
+function squareClicked(event) {
+    console.log("clicked", event);
+    const index = parseInt(event.target.id);
+    Squares[index].clickCount++;
+    console.log("squareClicked: ", index, Squares[index].clickCount);
 }
